9c05f4ad077a6c875d3ca77100cd5fc39b1a1f17
Spencer <sp797499@dal.ca>
2026-01-21 22:29:18 -0400

    add delete and setInCache for repo, repos are set as inCache when made

--- server/components/repo/repo.controller.js
+++ server/components/repo/repo.controller.js
@@ -1,28 +1,41 @@
 import { clone } from "isomorphic-git";
 import http from "isomorphic-git/http/node";
 import fs from "fs";
-import { getRepoIdByName, createRepo } from "../../models/repos.model.js";
+import { getRepoIdByName, createRepo, deleteRepo, setRepoInCache } from "../../models/repos.model.js";
+
+// export async function cloneRepo(req, res) {
+//     setRepoInCache(4, true);
+// }
 
 export async function cloneRepo(req, res) {
+    let newID = -1;
     try {
-
         const repoName = req.body.url;
-
         const id = await getRepoIdByName(repoName);
         if (id <= 0) {
+            //insert entry to database
             const newID = await createRepo(repoName);
+            if (newID <= -1) {
+                throw new Error("unsucessful database creation")
+            }
             const tmpDir = await fs.promises.mkdir(`./tmp/repo-${newID}`, { recursive: true });
 
+            // add/fix error handling when error is thrown here
             await clone({
                 fs,
                 http,
                 dir: tmpDir,
                 url: req.body.url,
             })
+
+            await setRepoInCache(newID, true);
         }
         
     } catch (error) {
-        return res.status(500).json({status: 500});
+        if (newID >= -1) {
+            await deleteRepo(newID);
+        }
+        return res.status(500).json({status: 500, error: error.message});
     }
     return res.status(200).json({status: 200});
 }
--- server/models/repos.model.js
+++ server/models/repos.model.js
@@ -16,9 +16,27 @@ export const getRepoIdByName = async (name) => {
 
 export const createRepo = async (name, repoPrivate=false) => {
     try {
-        const [data] = await db.execute(`INSERT INTO repos (name, inCache, private) VALUES ("${name}", false, ${repoPrivate})`);
-        return data.insertId
+        const [data] = await db.execute(`INSERT INTO repos (name, inCache, private) VALUES (?, false, ?)`, [name, repoPrivate]);
+        return data.insertId;
     } catch (err) {
-        return -1
+        return -1;
     }
 } 
+
+export const deleteRepo = async (id) => {
+    try {
+        const [data] = await db.execute(`DELETE FROM repos WHERE id = ?`, [id]);
+        return true;
+    } catch (err) {
+        return false;
+    }
+}
+
+export const setRepoInCache = async (id, inCache) => {
+    try {
+        const [data] = await db.execute(`UPDATE repos SET inCache = ? WHERE id = ?`, [inCache, id]);
+        return true;
+    } catch (err) {
+        return false;
+    }
+}
